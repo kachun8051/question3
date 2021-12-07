faffc83f5664fa6f65971d8ead4c4beb5b7dbe1b
kachun <kcwong8051@gmail.com>
2021-12-07 17:01:07 +0800

    keep in progress2

--- parsingXml.php
+++ parsingXml.php
@@ -2,5 +2,16 @@
     // create XML DOM tree
     $xml = new DOMDocument();
     $xml->load("question4.xml");
-    
+    $memberList = array();
+    $memberNodes = $xml->getElementsByTagName("member");
+    foreach ($memberNodes as $m) {
+        $category = $m->getAttribute("category");
+        $mid = $m->getElementsByTagName("mid")->item(0)->nodeValue;
+        $name = $m->getElementsByTagName("name")->item(0)->nodeValue;
+        $memberList[] = array("member" => array(
+            "category" => $category,
+            "mid" => $mid,
+            "name" => $name));        
+    }
+    echo json_encode($memberList);
 ?>
